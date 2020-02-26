2f2207b8cd765fb56172083fb78b792ec2ed502e
matrix618 <35847093+matrix618@users.noreply.github.com>
2020-02-26 22:14:50 +0800

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,18 @@
+import web
+ 
+urls = (
+    '/index/(.*)', 'index'
+    
+)
+ 
+app = web.application(urls, globals())
+render = web.template.render('templates/')
+ 
+class index:
+    def GET(self,name):
+        i = web.input(name=None,jj=None)
+        return render.index(i.name,i.jj)
+    
+ 
+if __name__ == "__main__":
+    app.run()
