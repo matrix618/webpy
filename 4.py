13835b7a762339cf98faa145f4c17e3a7cbb55ee
matrix618 <35847093+matrix618@users.noreply.github.com>
2020-02-26 23:43:01 +0800

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,20 @@
+import web
+ 
+urls = (
+    '/db/', 'index'
+    
+)
+ 
+app = web.application(urls, globals())
+render = web.template.render('templates/')
+db = web.database(dbn='mysql', host='127.0.0.1', port=3306,
+                  db='forum', user='root', pw='root')
+ 
+class index:
+    def GET(self):
+       email = db.select('mintest')
+       return render.index4(email)    
+    
+ 
+if __name__ == "__main__":
+    app.run()
