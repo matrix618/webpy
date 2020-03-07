14bec911d5f23a5f2ad567dd1872d2e64104ac5a
matrix618 <35847093+matrix618@users.noreply.github.com>
2020-03-07 17:18:20 +0800

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,26 @@
+import web
+ 
+urls = (
+    '/db/', 'index',
+    '/db/add','add'
+    
+)
+ 
+app = web.application(urls, globals())
+render = web.template.render('templates/')
+db = web.database(dbn='mysql', host='127.0.0.1', port=3306,
+                  db='forum', user='root', pw='root')
+ 
+class index:
+    def GET(self):
+       email = db.select('mintest')
+       return render.index5(email)
+
+class add:
+    def POST(self):
+        i=web.input()
+        n=db.insert('mintest',email=i.title,NAME=i.title)
+        raise web.seeother('/db/')
+ 
+if __name__ == "__main__":
+    app.run()
