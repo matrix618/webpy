9983a08efb67c5e5f474633efbc3e3439009dc69
gisempire <gisempire@foxmail.com>
2020-03-09 17:04:46 +0800

    Update 4.py

--- 4.py
+++ 4.py
@@ -8,7 +8,7 @@ urls = (
 app = web.application(urls, globals())
 render = web.template.render('templates/')
 db = web.database(dbn='mysql', host='127.0.0.1', port=3306,
-                  db='forum', user='root', pw='root')
+                  db='forum', user='root', pw='123456')
  
 class index:
     def GET(self):
