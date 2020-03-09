99a7ebf6c63a777d3025ff695be7dda8854ad87b
gisempire <gisempire@foxmail.com>
2020-03-09 17:04:49 +0800

    Update 5.py

--- 5.py
+++ 5.py
@@ -9,7 +9,7 @@ urls = (
 app = web.application(urls, globals())
 render = web.template.render('templates/')
 db = web.database(dbn='mysql', host='127.0.0.1', port=3306,
-                  db='forum', user='root', pw='root')
+                  db='forum', user='root', pw='123456')
  
 class index:
     def GET(self):
@@ -19,7 +19,7 @@ class index:
 class add:
     def POST(self):
         i=web.input()
-        n=db.insert('mintest',email=i.title,NAME=i.title)
+        n=db.insert('mintest',email=i.title,NAME=i.email)
         raise web.seeother('/db/')
  
 if __name__ == "__main__":
