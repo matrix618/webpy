cc7f7bd36befb6715786d87b9e2b0ce03ce788e6
gisempire <gisempire@foxmail.com>
2020-03-13 14:38:27 +0800

    Update 1b1.py

--- 1b1.py
+++ 1b1.py
@@ -5,17 +5,20 @@ import requests
 import json
 
 if __name__ == '__main__':
-    url = 'http://172.16.213.46:8080/pictureupload'
+    url = 'http://127.0.0.1:8080/pictureupload'
     t1 = "'1234567890','mix_0.jpg','mix_1.jpg','mix_2.jpg',{'Container': '1234567890','Matteral': [1], 'Quality': 0.8, 'plate': 11}"
     data = {'mykey': t1}
-    files = {'myfile':open(r'D:\ftp\CVIS_SW_IREC\picture\2020-03-03-BGV7100\bgp_png_V7100\202002220023600002S.png','rb')}
+
+    pa1 =r'\202002220023600002S.png'
+    pa2 =r'\202002220023600003S.png'
+    pa3 =r'\202002220023600004S.png'
+    
+    files = {'myfile':open(pa1,'rb'),'myfile1':open(pa2,'rb'),'myfile2':open(pa3,'rb')}
 ##    response = requests.request("POST",url,files=files)
     
 ##    response = requests.request("POST",url,data=data,files=files)
 
     response = requests.request("POST",url,data=data,files=files)
     
-
     print(response.text)
     
-
